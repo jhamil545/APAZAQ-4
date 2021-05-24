e8218a044cfc6f04e7a6ff41b7496f631c92da53
jhamil545 <jhamil.apaza@upeu.edu.pe>
2021-05-24 15:24:15 -0500

    sub programas

---
+++ ProyectoJN/src/App.java
@@ -0,0 +1,9 @@
+import pe.edu.upeu.SubPrograma;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        SubPrograma obsjn=new SubPrograma();
+        obsjn.calcularFuncionExponecial();
+    }
+}
