ee4fe998585b39fb67cb062b0e6af3354ad4dac1
jhamil545 <jhamil.apaza@upeu.edu.pe>
2021-05-26 18:39:05 -0500

    agregando documentacion interna

--- trabajo2/Ejercicio8.java
+++ trabajo2/Ejercicio8.java
@@ -1,10 +1,42 @@
 import java.util.Scanner;
-public class Ejercicio8 {
-    static Scanner teclado=new Scanner(System.in);
-    public static void descuentos() {
+class Ejercicio8{
+static Scanner teclado=new Scanner(System.in);
+static void descuentoproduc(){
+//Definir variables y otros
 
+int  nart=0, contador=0; 
+double precioj=0,desproduc=0;
+//datos de entrada
+
+
+System.out.println("¿Cuantos articulos comprara?");
+nart=teclado.nextInt();
+while (contador<=nart) {
+    System.out.println("cual es el precio del articulo");
+    precioj=teclado.nextInt();
+    if(precioj<=200){
+        precioj=precioj-(precioj*0.15);
+        desproduc=15;
         
-    }
+     }
+    else if (precioj>=100 && precioj<=200){
+        precioj=precioj-(precioj*0.12);
+        desproduc=12;
+        
+     }
+    else {
+        precioj=precioj-(precioj*0.10);
+        desproduc=10;
+        
+     }
+     contador++;
+}
 
-    
+ //Datos de salida:
+System.out.println("su descuento es de: "+desproduc);
+System.out.println("el costo total es;"+precioj);
+}
+public static void main(String[] arg){
+    descuentoproduc();
+}
 }
